3d1442347287c80b29a63d93e81303b37802bd51
movie-mate <45496302+movie-mate@users.noreply.github.com>
2018-12-08 22:39:59 +0300

    Update Script1.js

--- js/Script1.js
+++ js/Script1.js
@@ -7,7 +7,7 @@ $(window).on('load', function () {
 });
 $(document).ready(function () {
     $('.menu-activator').on('click', function () {
-        $('.menu-wrapper').toggle();
+        $('.menu-wrapper').toggle("slow");
     });
 });
 $(document).ready(function () {
